a458182cbe1cb82ef2ef2097b028895e0e37d3b8
jnprogrammer <joshuanparker@gmail.com>
2020-07-21 23:39:14 -0400

    Fleshed out api requests for getting books, adding them, and deleting them.

--- book/book.go
+++ book/book.go
@@ -2,20 +2,56 @@ package book
 
 import (
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	"github.com/jnprogrammer/go_fiber_restapi/database"
 )
 
+type Book struct {
+	gorm.Model
+	Title  string //'json:"title"'
+	Author string //'json:"author'
+	rating int    //'json:"rating"'
+}
+
 func GetBooks(c *fiber.Ctx) {
-	c.Send("All books")
+	db := database.DBConn
+	var books []Book
+	db.Find(&books)
+	c.JSON(books)
+	//c.Send("All books")
 }
 
 func GetBook(c *fiber.Ctx) {
-	c.Send("Single book")
+	id := c.Params("id")
+	db := database.DBConn
+	var book Book
+	db.Find(&book, id)
+	c.JSON(book)
+	//c.Send("Single book")
 }
 
 func NewBook(c *fiber.Ctx) {
+	db := database.DBConn
+	var book Book
+	book.Title = "1984"
+	book.Author = "George Orwell"
+	book.rating = 9001
+	db.Create(&book)
+	c.JSON(book)
 	c.Send("Adds a book")
 }
 
 func DeleteBooks(c *fiber.Ctx) {
 	c.Send("Deletes a book")
+	id := c.Params("id")
+	db := database.DBConn
+	var book Book
+	db.First(&book, id)
+	if book.Title == "" {
+		c.Status(500).Send("No book found with that ID")
+		return
+	}
+	db.Delete(&book)
+	c.Send("Book has been deleted")
+
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/jnprogrammer/go_fiber_restapi/book"
 	"github.com/jnprogrammer/go_fiber_restapi/database"
 )
@@ -15,13 +16,16 @@ func initDatabase() {
 		panic("Failed to connect to database connection")
 	}
 	fmt.Println("Database connection opened!")
+
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("Database Migrated")
 }
 
 func setupRoutes(app *fiber.App) {
 	app.Get("/api/v1/book", book.GetBooks)
 	app.Get("/api/v1/book/:id", book.GetBook)
-	app.Get("/api/v1/book", book.NewBook)
-	app.Get("/api/v1/book/:id", book.DeleteBooks)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBooks)
 
 }
 func main() {
